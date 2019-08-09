444b18684a99bf49bd1c15d41748f655c592df9a
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-08-09 11:52:08 -0400

    Refactored App to class component

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import "./App.css";
 import FormikForm from "./components/TheForm";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-      <FormikForm />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>App</h1>
+        <FormikForm />
+      </div>
+    );
+  }
 }
 
 export default App;

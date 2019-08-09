7944c9e3b27d0109ef0f6345b0c9cfaae757fbec
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-08-09 11:22:10 -0400

    Renamed form component so it doesn't conflict with formik

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
-import Form from "./components/Form";
+import TheForm from "./components/TheForm";
 
 function App() {
   return (
     <div className="App">
       <h1>App</h1>
-      <Form />
+      <TheForm />
     </div>
   );
 }
---
+++ client/src/components/TheForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+
+export default function TheForm({ values, errors, touched, status }) {
+  return (
+    <div>
+      <h1>Form</h1>
+    </div>
+  );
+}
